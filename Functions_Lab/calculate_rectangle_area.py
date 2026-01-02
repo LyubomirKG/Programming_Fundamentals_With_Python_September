5f3c32a9403b74207d770ee0e327c655d9dad6da
Lyubomir Georgiev <lyubomir.k.georgiev@gmail.com>
2026-01-02 13:04:50 +0200

    Create calculate_rectangle_area.py

---
+++ Functions_Lab/calculate_rectangle_area.py
@@ -0,0 +1,9 @@
+ef rectangle_area(width, height):
+    # Calculate area
+    area = width * height
+    return area
+
+# Example usage:
+width = int(input())
+height = int(input())
+print(rectangle_area(width, height))
