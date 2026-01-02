7b6b53cf97827b6a0fa9d9f4b96987614914cf87
Lyubomir Georgiev <lyubomir.k.georgiev@gmail.com>
2026-01-02 11:35:45 +0200

    Create Integer_operations.py

---
+++ Data_Types_And_Variables_Exercise/Integer_operations.py
@@ -0,0 +1,13 @@
+# Read four integers from the user
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+# Step-by-step calculation
+sum_ab = a + b
+divided = sum_ab // c
+result = divided * d
+
+# Print the final result
+print(result)
